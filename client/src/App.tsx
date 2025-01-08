138c458418a137d68df5eae79fed31553cc3fa1b
Fidal Mathew <fidal15perfect@gmail.com>
2025-01-08 19:08:54 +0530

    donation template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { StarknetProvider } from "@/StarknetProvider";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DonationStarknetEdit from "./pages/DonationStarknetEdit";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="donation" element={<DonationStarknetEdit />} />
             <Route path="*" element={<div>Not Found</div>} />
           </Routes>
         </BrowserRouter>
---
+++ client/src/pages/DonationStarknetEdit.tsx
@@ -0,0 +1,11 @@
+import DonationStarknet from "@/components/custom/DonationStarknet";
+
+function DonationStarknetEdit() {
+  return (
+    <div>
+      <DonationStarknet />
+    </div>
+  );
+}
+
+export default DonationStarknetEdit;
