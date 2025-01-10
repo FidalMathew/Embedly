dccbf7854d55e28f981acc8421b715fb7bd029fb
Fidal Mathew <fidal15perfect@gmail.com>
2025-01-10 22:24:49 +0530

    fix url addition issue

--- extension/content.js
+++ extension/content.js
@@ -6,7 +6,6 @@ async function replaceEmbTags() {
   console.log("Inside replaceEmbTags");
   const spans = document.querySelectorAll("span");
 
-  const fetchPromises = [];
   spans.forEach((span) => {
     const embRegex = /<emb\s*([^<]*)\s*emb>/g;
     let match;
@@ -15,21 +14,12 @@ async function replaceEmbTags() {
       .replace(/&gt;/g, ">");
 
     while ((match = embRegex.exec(decodedHTML)) !== null) {
-      let url = null;
       const match1 = match;
 
       console.log("MATCHHHHH: ", match1);
 
       const url1 = match[1].trim();
-
       console.log("URL1: ", url1);
-
-      // if (url1.startsWith("http")) url = url1;
-      // else if (url1.startsWith("ipfs://"))
-      //   url = "https://ipfs.io/ipfs/" + url1.substring("ipfs://".length);
-
-      // console.log(`Fetching URL: ${url}`); // Debugging information
-      // if (!url) continue;
 
       const defaultResponse = {
         span: "defaultSpan",
@@ -62,7 +52,7 @@ async function replaceEmbTags() {
 
 <body>
     <div class="iframe-container">
-        <iframe src="http://localhost:5173/custom/bafkreigoipk2irrkvdiyst3zpe53xaeozzewe2ki7w5op7svlqg634i74u" title="Embedded React App"
+        <iframe src="http://localhost:5173/custom/${url1}" title="Embedded React App"
             allow="accelerometer; camera; encrypted-media; geolocation; gyroscope; microphone"
             sandbox="allow-same-origin allow-scripts allow-forms"></iframe>
     </div>
